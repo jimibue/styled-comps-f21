d8f589b86d6f698803af82334870116af864e82c
James Yeates <jimibue@gmail.com>
2021-09-14 11:41:26 -0600

    repos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import "./App.css";
-import { Container, Header, Segment } from "semantic-ui-react";
+import {
+  Card,
+  Container,
+  Grid,
+  GridRow,
+  Header,
+  Segment,
+} from "semantic-ui-react";
 import styled from "styled-components";
 import HeaderText from "./components/HeaderText";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
+    // onmount
+    getRepos();
+    // this function would be call on unmount
+    // return () => {
+    //   console.log("unmount");
+    // };
+  }, []);
+
+  const getRepos = async () => {
+    try {
+      let res = await axios.get(
+        "https://api.github.com/users/facebook/repos?sort=created"
+      );
+      setRepos(res.data);
+    } catch (err) {}
+  };
+
+  const renderRepos = () => {
+    return repos.map((r) => {
+      return (
+        <Grid.Column key={r.id} width={4}>
+          <Card>
+            <Card.Content>
+              <Card.Header>{r.name}</Card.Header>
+            </Card.Content>
+            <Card.Meta>{r.description}</Card.Meta>
+          </Card>
+        </Grid.Column>
+      );
+    });
+  };
+
   return (
     <AppContainer>
       <HeaderText size="large">Header Text component</HeaderText>
       <Segment as={Transparent}>
         <HeaderText size="med">My Projects</HeaderText>
+        <Grid>
+          <Grid.Row>{renderRepos()}</Grid.Row>
+        </Grid>
       </Segment>
       <Segment as={Transparent}>
         <HeaderText>Contacts</HeaderText>
