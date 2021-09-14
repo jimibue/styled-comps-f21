5eb3f5987eae2636abdf147062f243e00f1f1371
James Yeates <jimibue@gmail.com>
2021-09-14 11:23:05 -0600

    header demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import "./App.css";
-import { Container, Header } from "semantic-ui-react";
+import { Container, Header, Segment } from "semantic-ui-react";
+import styled from "styled-components";
+import HeaderText from "./components/HeaderText";
 
 function App() {
   return (
-    <Container>
-      <Header>Yo</Header>
-    </Container>
+    <AppContainer>
+      <HeaderText size="large">Header Text component</HeaderText>
+      <Segment as={Transparent}>
+        <HeaderText size="med">My Projects</HeaderText>
+      </Segment>
+      <Segment as={Transparent}>
+        <HeaderText>Contacts</HeaderText>
+      </Segment>
+    </AppContainer>
   );
 }
+
+const Transparent = styled.div`
+  background: transparent !important;
+`;
+const AppContainer = styled.div`
+  min-height: 100vh;
+  background: rgb(253, 75, 29);
+  background: linear-gradient(
+    90deg,
+    rgba(253, 75, 29, 1) 5%,
+    rgba(252, 153, 69, 1) 100%
+  );
+`;
 
 export default App;
